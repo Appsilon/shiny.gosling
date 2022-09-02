a05691b2d02e43b0b5b57300c4d4df578c1d2721
federiva <federivadeneira@gmail.com>
2022-09-02 12:01:38 -0300

    build: Updating config

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -10,6 +10,16 @@ const config = {
   module: {
     rules: [
       {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
         test: /\.css$/,
         use: [
           'style-loader',
