ec1e515a53600eef201e38aea326b4797ac102de
Anirban Shaw <anirbanshaw24@gmail.com>
2022-09-03 19:23:03 +0530

    fix: refactor shiny example

--- inst/examples/multiTrackApp/server.R
+++ inst/examples/multiTrackApp/server.R
@@ -1,121 +1,139 @@
 function(input, output, session) {
   all_reactive_values <- reactiveValues(
     composed_track = NULL,
-    composed_views = NULL
+    composed_views = NULL,
+    track1 = NULL,
+    track2 = NULL,
+    track3 = NULL
   )
   observeEvent(input$layout, {
-    track1 <- add_single_track(
-      id = "track2",
-      title = "Patient Overview (PD35930a)",
-      alignment = "overlay",
-      data = track_data(
-        url = "https://raw.githubusercontent.com/sehilyi/gemini-datasets/master/data/UCSC.HG38.Human.CytoBandIdeogram.csv",
-        type = "csv",
-        chromosomeField = "Chromosome",
-        genomicFields = c("chromStart", "chromEnd")
-      ),
-      tracks = add_multi_tracks(
-        add_single_track(
-          mark = "rect"
-        ),
-        add_single_track(
-          mark = "brush",
-          x = visual_channel_x(linkingId = "mid-scale"),
-          strokeWidth = 1.5, stroke = "#0070DC",
-          color = "#AFD8FF", opacity = 0.5
-        )
-      ),
-      color = visual_channel_color(
-        field = "Stain", type = "nominal", domain = c(
-          "gneg", "gpos25", "gpos50", "gpos75", "gpos100", "gvar", "acen"
-        ), range = c(
-          "white", "lightgray", "gray", "gray", "black", "#7B9CC8", "#DC4542"
-        )
-      ),
-      size = 18,
-      x = visual_channel_x(field = "chromStart", type = "genomic"),
-      xe = visual_channel_x(field = "chromEnd", type = "genomic"),
-      stroke = "gray", strokeWidth = 0.3,
-      width = 500, height = 100
+    track1_data <- track_data(
+      url = "https://raw.githubusercontent.com/sehilyi/gemini-datasets/master/data/UCSC.HG38.Human.CytoBandIdeogram.csv",
+      type = "csv",
+      chromosomeField = "Chromosome",
+      genomicFields = c("chromStart", "chromEnd")
     )
 
-    track2 <- add_single_track(
-      id = "track3",
-      title = "Putative Driver",
-      alignment = "overlay",
-      data = track_data(
-        url = "inst/examples/multiTrackApp/data/driver.df.scanb.complete.csv",
-        type = "csv",
-        chromosomeField = "Chr",
-        genomicFields = c("ChrStart", "ChrEnd")
+    track_1_tracks <- add_multi_tracks(
+      add_single_track(
+        mark = "rect"
       ),
-      dataTransform = track_data_transform(
-        type = "filter", field = "Sample", oneOf = list("PD35930a")
-      ),
-      tracks = add_multi_tracks(
-        add_single_track(
-          mark = "text"
-        ),
-        add_single_track(
-          mark = "triangleBottom", size = 5
-        )
-      ),
-      x = visual_channel_x(field = "ChrStart", type = "genomic"),
-      xe = visual_channel_x(field = "ChrEnd", type = "genomic"),
-      text = visual_channel_text(field = "Gene", type = "nominal"),
-      color = "black",
-      style = default_track_styles(
-        textFontWeight = "normal", dx = -10, outlineWidth = 0
-      ),
-      width = 500, height = 40
+      add_single_track(
+        mark = "brush",
+        x = visual_channel_x(linkingId = "mid-scale"),
+        strokeWidth = 1.5, stroke = "#0070DC",
+        color = "#AFD8FF", opacity = 0.5
+      )
     )
 
-    track3 <- add_single_track(
-      id = "track3",
-      title = "LOH",
-      style = default_track_styles(
-        background = "lightgray", backgroundOpacity = 0.2
-      ),
-      alignment = "overlay",
-      data = track_data(
-        url = "https://s3.amazonaws.com/gosling-lang.org/data/cancer/cnv.PD35930a.csv",
-        type = "csv",
-        headerNames = c(
-          "id",
-          "chr",
-          "start",
-          "end",
-          "total_cn_normal",
-          "minor_cp_normal",
-          "total_cn_tumor",
-          "minor_cn_tumor"
-        ),
-        chromosomeField = "chr",
-        genomicFields = c("start", "end")
-      ),
-      dataTransform = track_data_transform(
-        type = "filter", field = "minor_cn_tumor", oneOf = list("0")
-      ),
-      tracks = add_multi_tracks(
-        add_single_track(
-          mark = "rect"
-        ),
-        add_single_track(
-          mark = "brush", x = visual_channel_x(linkingId = "mid-scale"),
-          strokeWidth = 1, stroke = "#94C2EF",
-          color = "#AFD8FF"
-        )
-      ),
-      x = visual_channel_x(field = "start", type = "genomic"),
-      xe = visual_channel_x(field = "end", type = "genomic"),
-      color = "#FB6A4B",
-      width = 620, height = 40
+    track1_color <- visual_channel_color(
+      field = "Stain", type = "nominal", domain = c(
+        "gneg", "gpos25", "gpos50", "gpos75", "gpos100", "gvar", "acen"
+      ), range = c(
+        "white", "lightgray", "gray", "gray", "black", "#7B9CC8", "#DC4542"
+      )
     )
 
+    track1_x <- visual_channel_x(field = "chromStart", type = "genomic")
+    track1_xe <- visual_channel_x(field = "chromEnd", type = "genomic")
 
+    all_reactive_values$track1 <- add_single_track(
+      id = "track2", title = "Patient Overview (PD35930a)",
+      alignment = "overlay", data = track1_data, tracks = track_1_tracks,
+      color = track1_color, size = 18, x = track1_x, xe = track1_xe,
+      stroke = "gray", strokeWidth = 0.3, width = 500, height = 100
+    )
+  })
+
+  observeEvent(input$layout, {
+    track2_data <- track_data(
+      url = "inst/examples/multiTrackApp/data/driver.df.scanb.complete.csv",
+      type = "csv",
+      chromosomeField = "Chr",
+      genomicFields = c("ChrStart", "ChrEnd")
+    )
+
+    track2_data_transform <- track_data_transform(
+      type = "filter", field = "Sample", oneOf = list("PD35930a")
+    )
+
+    track2_tracks <- add_multi_tracks(
+      add_single_track(
+        mark = "text"
+      ),
+      add_single_track(
+        mark = "triangleBottom", size = 5
+      )
+    )
+
+    track2_x <- visual_channel_x(field = "ChrStart", type = "genomic")
+    track2_xe <- visual_channel_x(field = "ChrEnd", type = "genomic")
+
+    track2_text <- visual_channel_text(field = "Gene", type = "nominal")
+    track2_style <- default_track_styles(
+      textFontWeight = "normal", dx = -10, outlineWidth = 0
+    )
+    all_reactive_values$track2 <- add_single_track(
+      id = "track3", title = "Putative Driver",
+      alignment = "overlay", data = track2_data,
+      dataTransform = track2_data_transform, tracks = track2_tracks,
+      x = track2_x, xe = track2_xe, text = track2_text,
+      color = "black", style = track2_style, width = 500, height = 40
+    )
+  })
+
+  observeEvent(input$layout, {
+
+    track3_styles <- default_track_styles(
+      background = "lightgray", backgroundOpacity = 0.2
+    )
+    track3_data <- track_data(
+      url = "https://s3.amazonaws.com/gosling-lang.org/data/cancer/cnv.PD35930a.csv",
+      type = "csv",
+      headerNames = c(
+        "id",
+        "chr",
+        "start",
+        "end",
+        "total_cn_normal",
+        "minor_cp_normal",
+        "total_cn_tumor",
+        "minor_cn_tumor"
+      ),
+      chromosomeField = "chr",
+      genomicFields = c("start", "end")
+    )
+    track3_data_transform <- track_data_transform(
+      type = "filter", field = "minor_cn_tumor", oneOf = list("0")
+    )
+    track3_tracks <- add_multi_tracks(
+      add_single_track(
+        mark = "rect"
+      ),
+      add_single_track(
+        mark = "brush", x = visual_channel_x(linkingId = "mid-scale"),
+        strokeWidth = 1, stroke = "#94C2EF",
+        color = "#AFD8FF"
+      )
+    )
+    track3_x <- visual_channel_x(field = "start", type = "genomic")
+    track3_xe <- visual_channel_x(field = "end", type = "genomic")
+    all_reactive_values$track3 <- add_single_track(
+      id = "track3", title = "LOH",
+      style = track3_styles, alignment = "overlay",
+      data = track3_data, dataTransform = track3_data_transform,
+      tracks = track3_tracks, x = track3_x, xe = track3_xe,
+      color = "#FB6A4B", width = 620, height = 40
+    )
+  })
+
+  observeEvent(input$layout, {
     all_reactive_values$composed_track <- compose_single_track_view(
       multi = TRUE,
-      tracks = add_multi_tracks(track1, track2, track3),
+      tracks = add_multi_tracks(
+        all_reactive_values$track1, all_reactive_values$track2,
+        all_reactive_values$track3
+      ),
       xOffset = 190, layout = input$layout, spacing = 1
     )
     all_reactive_values$composed_views <- arrange_views(
