94de23a32fee4bc8b9c9669b1484b2e352f4dabe
federiva <federivadeneira@gmail.com>
2022-11-10 11:28:22 -0300

    chore: reverting build

---
+++ R/export_png.R
@@ -0,0 +1,18 @@
+#' Export PNG
+#' @description
+#' Exports PNG
+#' @param component_id A character. The id of the component_id prop passed to the
+#' GoslingComponent function.
+#' @param transparent_background A Boolean. Determine if the background
+#' should be transparent or not (Default: false).
+#' @importFrom shiny getDefaultReactiveDomain
+#' @export
+export_png <- function(component_id, transparent_background = FALSE, session = getDefaultReactiveDomain()) {
+  session$sendCustomMessage(
+    'export_png',
+    list(
+      component_id = component_id,
+      transparent_background = transparent_background
+    )
+  )
+}
