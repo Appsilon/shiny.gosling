ecdbd5549d8b07e2ae601c8819cb09316054398d
Anirban Shaw <anirbanshaw24@gmail.com>
2022-11-15 00:57:20 +0530

    doc: undo function delete

--- R/utils.R
+++ R/utils.R
@@ -169,6 +169,102 @@ atomic_values_to_list <- function(property_list) {
 #' Add this function at the begining of ui. This is needed for
 #' gosling to work in shiny plots.
 #'
+#' @examples
+#' if(interactive()) {
+#'   library(shiny)
+#'   library(shiny.gosling)
+#'
+#'   track5_styles <- default_track_styles(
+#'     legendTitle = "SV Class"
+#'   )
+#'   track5_data <- track_data(
+#'     url = "https://s3.amazonaws.com/gosling-lang.org/data/cancer/rearrangement.PD35930a.csv",
+#'     type = "csv",
+#'     genomicFieldsToConvert = json_list(
+#'       json_list(
+#'         chromosomeField = "chr1",
+#'         genomicFields = c("start1", "end1")
+#'       ),
+#'       json_list(
+#'         chromosomeField = "chr2",
+#'         genomicFields = c("start2", "end2")
+#'       )
+#'     )
+#'   )
+#'   track5_tracks <- add_multi_tracks(
+#'     add_single_track(
+#'       mark = "rect"
+#'     ),
+#'     add_single_track(
+#'       mark = "withinLink", x = visual_channel_x(linkingId = "mid-scale"),
+#'       strokeWidth = 0
+#'     )
+#'   )
+#'   track5_color <- visual_channel_color(
+#'     field = "svclass",
+#'     type = "nominal",
+#'     legend = TRUE,
+#'     domain = json_list(
+#'       "tandem-duplication", "translocation", "delection", "inversion"
+#'     ),
+#'     range = json_list(
+#'       "#569C4D", "#4C75A2", "#DA5456", "#EA8A2A"
+#'     )
+#'   )
+#'   track5_stroke <- visual_channel_stroke(
+#'     field = "svclass",
+#'     type = "nominal",
+#'     domain = json_list(
+#'       "tandem-duplication", "translocation", "delection", "inversion"
+#'     ),
+#'     range = json_list(
+#'       "#569C4D", "#4C75A2", "#DA5456", "#EA8A2A"
+#'     )
+#'   )
+#'   track5_x <- visual_channel_x(field = "start1", type = "genomic")
+#'   track5_xe <- visual_channel_x(field = "end2", type = "genomic")
+#'   track5 <- add_single_track(
+#'     id = "track5", title = "Structural Variant",
+#'     data = track5_data, mark = "withinLink",
+#'     x = track5_x, xe = track5_xe,
+#'     color = track5_color, width = 500, height = 80, stroke = track5_stroke,
+#'     strokeWidth = 1, opacity = 0.6, style = track5_styles
+#'   )
+#'
+#'   composed_track <- compose_view(
+#'     multi = TRUE,
+#'     tracks = add_multi_tracks(
+#'       track5
+#'     ),
+#'     xOffset = 190, layout = "circular", spacing = 1
+#'   )
+#'
+#'   composed_views <- arrange_views(
+#'     views = composed_track,
+#'     arrangement = "vertical"
+#'   )
+#'
+#'   ui <- fluidPage(
+#'     use_gosling(),
+#'     fluidRow(
+#'       column(6, goslingOutput("gosling_plot"))
+#'     )
+#'   )
+#'
+#'
+#'   server <- function(input, output, session) {
+#'     output$gosling_plot <- renderGosling({
+#'       gosling(
+#'         component_id = "component_2",
+#'         composed_views, clean_braces = FALSE
+#'       )
+#'     })
+#'   }
+#'
+#'   shinyApp(ui, server)
+#'
+#' }
+#'
 #' @return Gosling initiator HTML.
 #' @export
 use_gosling <- function() {
@@ -188,6 +284,105 @@ use_gosling <- function() {
 #' @param composed_views The views composed with arrange_views.
 #' @param clean_braces Whether to remove extra square brackets from the
 #' json string.
+#'
+#' @examples
+#' if(interactive()) {
+#'   library(shiny)
+#'   library(shiny.gosling)
+#'
+#'   # Circular track 1 ----
+#'   circular_track1_data <- track_data(
+#'     url = "https://server.gosling-lang.org/api/v1/tileset_info/?d=cistrome-multivec",
+#'     type = "multivec",
+#'     row = "sample",
+#'     column = "position",
+#'     value = "peak",
+#'     categories = c("sample 1", "sample 2", "sample 3", "sample 4"),
+#'     binSize = 4
+#'   )
+#'
+#'   circular_track1_x <- visual_channel_x(field = "start", type = "genomic")
+#'   circular_track1_xe <- visual_channel_x(field = "end", type = "genomic")
+#'
+#'   circular_track1_y <- visual_channel_y(field = "peak", type = "quantitative")
+#'
+#'   circular_track1_row <- visual_channel_row(
+#'     field = "sample", type = "nominal"
+#'   )
+#'
+#'   circular_track1_color <- visual_channel_color(
+#'     field = "sample", type = "nominal"
+#'   )
+#'
+#'   circular_track1_tracks <- add_multi_tracks(
+#'     add_single_track(
+#'       mark = "bar"
+#'     ),
+#'     add_single_track(
+#'       mark = "brush",
+#'       x = visual_channel_x(linkingId = "detail-1"),
+#'       color = "blue"
+#'     ),
+#'     add_single_track(
+#'       mark = "brush",
+#'       x = visual_channel_x(linkingId = "detail-2"),
+#'       color = "red"
+#'     )
+#'   )
+#'
+#'   circular_track1_styles <- default_track_styles(
+#'     outlineWidth = 0
+#'   )
+#'
+#'   circular_track1 <- add_single_track(
+#'     id = "circular_track1", alignment = "overlay", data = circular_track1_data,
+#'     x = circular_track1_x, xe = circular_track1_xe,
+#'     y = circular_track1_y, row = circular_track1_row,
+#'     color = circular_track1_color,
+#'     stroke =  "black", strokeWidth = 0.3,
+#'     tracks = circular_track1_tracks,
+#'     style = circular_track1_styles,
+#'     width = 500, height = 100
+#'   )
+#'
+#'   # Compose Circular track ----
+#'   circular_composed_view <- compose_view(
+#'     multi = TRUE,
+#'     tracks = add_multi_tracks(
+#'       circular_track1
+#'     ),
+#'     static = TRUE, layout = "circular", alignment = "stack"
+#'   )
+#'
+#'   # Arrange final view
+#'   circular_linear_view <- arrange_views(
+#'     arrangement = "horizontal",
+#'     views = list(circular_composed_view)
+#'   )
+#'
+#'
+#'
+#'
+#'   ui <- fluidPage(
+#'     use_gosling(),
+#'     fluidRow(
+#'       column(6, goslingOutput("gosling_plot"))
+#'     )
+#'   )
+#'
+#'
+#'   server <- function(input, output, session) {
+#'     output$gosling_plot <- renderGosling({
+#'       gosling(
+#'         component_id = "circular_component",
+#'         circular_linear_view, clean_braces = FALSE
+#'       )
+#'     })
+#'   }
+#'
+#'   shinyApp(ui, server)
+#'
+#' }
 #'
 #' @return Gosling component for rendering on R shiny apps
 #' @export
