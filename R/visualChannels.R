f68cac9a118acee5a8d9b83489efefaad88434af
Anirban Shaw <anirbanshaw24@gmail.com>
2022-11-14 23:58:02 +0530

    fix: visual_channel uncomment

--- R/visualChannels.R
+++ R/visualChannels.R
@@ -30,13 +30,107 @@ visual_channel <- function(
 #' @param aggregate A character. One of "max", "min", "mean", "bin", "count".
 #' Specify how to aggregate data. Default: undefined.
 #' @param ... Any other parameters to pass to gosling.js.
+#' @examples
+#' if(interactive()) {
+#'   library(shiny)
+#'   library(shiny.gosling)
+#'
+#'   cistrome_data <-
+#'     "https://server.gosling-lang.org/api/v1/tileset_info/?d=cistrome-multivec"
+#'
+#'   single_track <- add_single_track(
+#'     id = "track1",
+#'     data = track_data(
+#'       url = cistrome_data,
+#'       type = "multivec",
+#'       row = "sample",
+#'       column = "position",
+#'       value = "peak",
+#'       categories = c("sample 1", "sample 2", "sample 3", "sample 4"),
+#'       binSize = 4,
+#'     ),
+#'     mark = "rect",
+#'     x = visual_channel_x(field = "start", type = "genomic", axis = "top"),
+#'     xe = visual_channel_x(field = "end", type = "genomic"),
+#'     row = visual_channel_row(
+#'       field = "sample",
+#'       type = "nominal",
+#'       legend = TRUE
+#'     ),
+#'     color = visual_channel_color(
+#'       field = "peak",
+#'       type = "quantitative",
+#'       legend = TRUE
+#'     ),
+#'     tooltip = visual_channel_tooltips(
+#'       visual_channel_tooltip(field = "start", type = "genomic",
+#'                              alt = "Start Position"),
+#'       visual_channel_tooltip(field = "end", type = "genomic",
+#'                              alt = "End Position"),
+#'       visual_channel_tooltip(
+#'         field = "peak",
+#'         type = "quantitative",
+#'         alt = "Value",
+#'         format = "0.2"
+#'       )
+#'     ),
+#'     width = 600,
+#'     height = 130
+#'   )
+#'
+#'   single_composed_track <- compose_view(
+#'     tracks = single_track
+#'   )
+#'
+#'   single_composed_views <- arrange_views(
+#'     title = "Single Track",
+#'     subtitle = "This is the simplest single track visualization with a linear layout",
+#'     layout = "circular", #"linear"
+#'     views = single_composed_track,
+#'     xDomain = list(
+#'       chromosome = "chr1",
+#'       interval = c(1, 3000500)
+#'     )
+#'   )
+#'
+#'   ui <- fluidPage(
+#'     use_gosling(),
+#'     fluidRow(
+#'       column(6, goslingOutput("gosling_plot")),
+#'       column(
+#'         1, br(), actionButton(
+#'           "download_pdf",
+#'           "PDF",
+#'           icon = icon("cloud-arrow-down")
+#'         )
+#'       )
+#'     )
+#'   )
+#'
+#'
+#'   server <- function(input, output, session) {
+#'     output$gosling_plot <- renderGosling({
+#'       gosling(
+#'         component_id = "component_1",
+#'         single_composed_views,
+#'         clean_braces = TRUE
+#'       )
+#'     })
+#'
+#'     observeEvent(input$download_pdf, {
+#'       export_pdf(component_id = "component_1")
+#'     })
+#'   }
+#'
+#'   shinyApp(ui, server)
+#'
+#' }
 #'
 #' @details For more info visit
 #' http://gosling-lang.org/docs/visual-channel#x--xe
 #'
 #' @return List object.
 #' @export
-#'
 visual_channel_x <- function(
     field = NULL, type = NULL, legend = NULL, grid = NULL,
     axis = NULL,
@@ -66,6 +160,105 @@ visual_channel_x <- function(
 #' @param aggregate A character. One of "max", "min", "mean", "bin", "count".
 #' Specify how to aggregate data. Default: undefined.
 #' @param ... Any other parameters to pass to gosling.js.
+#' @examples
+#' if(interactive()) {
+#'   library(shiny)
+#'   library(shiny.gosling)
+#'
+#'   # Circular track 1 ----
+#'   circular_track1_data <- track_data(
+#'     url = "https://server.gosling-lang.org/api/v1/tileset_info/?d=cistrome-multivec",
+#'     type = "multivec",
+#'     row = "sample",
+#'     column = "position",
+#'     value = "peak",
+#'     categories = c("sample 1", "sample 2", "sample 3", "sample 4"),
+#'     binSize = 4
+#'   )
+#'
+#'   circular_track1_x <- visual_channel_x(field = "start", type = "genomic")
+#'   circular_track1_xe <- visual_channel_x(field = "end", type = "genomic")
+#'
+#'   circular_track1_y <- visual_channel_y(field = "peak", type = "quantitative")
+#'
+#'   circular_track1_row <- visual_channel_row(
+#'     field = "sample", type = "nominal"
+#'   )
+#'
+#'   circular_track1_color <- visual_channel_color(
+#'     field = "sample", type = "nominal"
+#'   )
+#'
+#'   circular_track1_tracks <- add_multi_tracks(
+#'     add_single_track(
+#'       mark = "bar"
+#'     ),
+#'     add_single_track(
+#'       mark = "brush",
+#'       x = visual_channel_x(linkingId = "detail-1"),
+#'       color = "blue"
+#'     ),
+#'     add_single_track(
+#'       mark = "brush",
+#'       x = visual_channel_x(linkingId = "detail-2"),
+#'       color = "red"
+#'     )
+#'   )
+#'
+#'   circular_track1_styles <- default_track_styles(
+#'     outlineWidth = 0
+#'   )
+#'
+#'   circular_track1 <- add_single_track(
+#'     id = "circular_track1", alignment = "overlay", data = circular_track1_data,
+#'     x = circular_track1_x, xe = circular_track1_xe,
+#'     y = circular_track1_y, row = circular_track1_row,
+#'     color = circular_track1_color,
+#'     stroke =  "black", strokeWidth = 0.3,
+#'     tracks = circular_track1_tracks,
+#'     style = circular_track1_styles,
+#'     width = 500, height = 100
+#'   )
+#'
+#'   # Compose Circular track ----
+#'   circular_composed_view <- compose_view(
+#'     multi = TRUE,
+#'     tracks = add_multi_tracks(
+#'       circular_track1
+#'     ),
+#'     static = TRUE, layout = "circular", alignment = "stack"
+#'   )
+#'
+#'   # Arrange final view
+#'   circular_linear_view <- arrange_views(
+#'     arrangement = "horizontal",
+#'     views = list(circular_composed_view)
+#'   )
+#'
+#'
+#'
+#'
+#'   ui <- fluidPage(
+#'     use_gosling(),
+#'     fluidRow(
+#'       column(6, goslingOutput("gosling_plot"))
+#'     )
+#'   )
+#'
+#'
+#'   server <- function(input, output, session) {
+#'     output$gosling_plot <- renderGosling({
+#'       gosling(
+#'         component_id = "circular_component",
+#'         circular_linear_view, clean_braces = FALSE
+#'       )
+#'     })
+#'   }
+#'
+#'   shinyApp(ui, server)
+#'
+#' }
+#'
 #'
 #' @details For more info visit
 #' http://gosling-lang.org/docs/visual-channel#y--ye
@@ -102,6 +295,104 @@ visual_channel_y <- function(
 #' @param aggregate A character. One of "max", "min", "mean", "bin", "count".
 #' Specify how to aggregate data. Default: undefined.
 #' @param ... Any other parameters to pass to gosling.js.
+#' @examples
+#' if(interactive()) {
+#'   library(shiny)
+#'   library(shiny.gosling)
+#'
+#'   # Circular track 1 ----
+#'   circular_track1_data <- track_data(
+#'     url = "https://server.gosling-lang.org/api/v1/tileset_info/?d=cistrome-multivec",
+#'     type = "multivec",
+#'     row = "sample",
+#'     column = "position",
+#'     value = "peak",
+#'     categories = c("sample 1", "sample 2", "sample 3", "sample 4"),
+#'     binSize = 4
+#'   )
+#'
+#'   circular_track1_x <- visual_channel_x(field = "start", type = "genomic")
+#'   circular_track1_xe <- visual_channel_x(field = "end", type = "genomic")
+#'
+#'   circular_track1_y <- visual_channel_y(field = "peak", type = "quantitative")
+#'
+#'   circular_track1_row <- visual_channel_row(
+#'     field = "sample", type = "nominal"
+#'   )
+#'
+#'   circular_track1_color <- visual_channel_color(
+#'     field = "sample", type = "nominal"
+#'   )
+#'
+#'   circular_track1_tracks <- add_multi_tracks(
+#'     add_single_track(
+#'       mark = "bar"
+#'     ),
+#'     add_single_track(
+#'       mark = "brush",
+#'       x = visual_channel_x(linkingId = "detail-1"),
+#'       color = "blue"
+#'     ),
+#'     add_single_track(
+#'       mark = "brush",
+#'       x = visual_channel_x(linkingId = "detail-2"),
+#'       color = "red"
+#'     )
+#'   )
+#'
+#'   circular_track1_styles <- default_track_styles(
+#'     outlineWidth = 0
+#'   )
+#'
+#'   circular_track1 <- add_single_track(
+#'     id = "circular_track1", alignment = "overlay", data = circular_track1_data,
+#'     x = circular_track1_x, xe = circular_track1_xe,
+#'     y = circular_track1_y, row = circular_track1_row,
+#'     color = circular_track1_color,
+#'     stroke =  "black", strokeWidth = 0.3,
+#'     tracks = circular_track1_tracks,
+#'     style = circular_track1_styles,
+#'     width = 500, height = 100
+#'   )
+#'
+#'   # Compose Circular track ----
+#'   circular_composed_view <- compose_view(
+#'     multi = TRUE,
+#'     tracks = add_multi_tracks(
+#'       circular_track1
+#'     ),
+#'     static = TRUE, layout = "circular", alignment = "stack"
+#'   )
+#'
+#'   # Arrange final view
+#'   circular_linear_view <- arrange_views(
+#'     arrangement = "horizontal",
+#'     views = list(circular_composed_view)
+#'   )
+#'
+#'
+#'
+#'
+#'   ui <- fluidPage(
+#'     use_gosling(),
+#'     fluidRow(
+#'       column(6, goslingOutput("gosling_plot"))
+#'     )
+#'   )
+#'
+#'
+#'   server <- function(input, output, session) {
+#'     output$gosling_plot <- renderGosling({
+#'       gosling(
+#'         component_id = "circular_component",
+#'         circular_linear_view, clean_braces = FALSE
+#'       )
+#'     })
+#'   }
+#'
+#'   shinyApp(ui, server)
+#'
+#' }
 #'
 #' @details For more info visit
 #' http://gosling-lang.org/docs/visual-channel#row
@@ -152,6 +443,136 @@ visual_channel_size <- function(
 #' @param range A vector of characters or numbers. Values of the visual channel.
 #' @param domain A vector of characters or numbers. Values of the data.
 #' @param ... Any other parameters to pass to gosling.js.
+#' @examples
+#' if(interactive()) {
+#'   library(shiny)
+#'   library(shiny.gosling)
+#'
+#'   # View 2 Track 3----
+#'   view2_track3_data <- track_data(
+#'     url = "https://server.gosling-lang.org/api/v1/tileset_info/?d=NC_045512_2-multivec",
+#'     type = "multivec",
+#'     row = "base",
+#'     column = "position",
+#'     value = "count",
+#'     categories = c("A", "T", "G", "C"),
+#'     start = "start",
+#'     end = "end"
+#'   )
+#'
+#'   view2_track3a <- add_single_track(
+#'     mark = "bar",
+#'     y = visual_channel_y(
+#'       field = "count", type = "quantitative", axis = "none"
+#'     )
+#'   )
+#'
+#'   view2_track3b <- add_single_track(
+#'     dataTransform = track_data_transform(
+#'       type = "filter",
+#'       field = "count",
+#'       oneOf = list(0),
+#'       not = TRUE
+#'     ),
+#'     mark = "text",
+#'     x = visual_channel_x(
+#'       field = "start", type = "genomic"
+#'     ),
+#'     xe = visual_channel_x(
+#'       field = "end", type = "genomic"
+#'     ),
+#'     size = 24,
+#'     color = "white",
+#'     visibility = list(list(
+#'       operation = "less-than",
+#'       measure = "width",
+#'       threshold = "|xe-x|",
+#'       transitionPadding = 30,
+#'       target = "mark"
+#'     ),
+#'     list(
+#'       operation = "LT",
+#'       measure = "zoomLevel",
+#'       threshold = 40,
+#'       target = "track"
+#'     ))
+#'   )
+#'
+#'   view2_track3_x <- visual_channel_x(
+#'     field = "position", type = "genomic"
+#'   )
+#'
+#'   view2_track3_color <- visual_channel_color(
+#'     field = "base",
+#'     type = "nominal",
+#'     domain = c("A", "T", "G", "C"),
+#'     legend = TRUE
+#'   )
+#'
+#'   view2_track3_text <- visual_channel_text(
+#'     field = "base", type = "nominal"
+#'   )
+#'
+#'   view2_track3_style <- default_track_styles(
+#'     inlineLegend = TRUE
+#'   )
+#'
+#'   view2_track3 <- add_single_track(
+#'     title = "NC_045512.2 Sequence",
+#'     alignment = "overlay",
+#'     data = view2_track3_data,
+#'     tracks = add_multi_tracks(
+#'       view2_track3a, view2_track3b
+#'     ),
+#'     x = view2_track3_x,
+#'     color = view2_track3_color,
+#'     text = view2_track3_text,
+#'     style = view2_track3_style,
+#'     width = 800, height = 40
+#'   )
+#'
+#'   view2 <- compose_view(
+#'     multi = TRUE,
+#'     centerRadius = 0,
+#'     xDomain = list(interval = c(1, 29903)),
+#'     linkingId = "detail",
+#'     alignment = "stack",
+#'     tracks = add_multi_tracks(
+#'       view2_track3
+#'     )
+#'   )
+#'
+#'   combined_view <- arrange_views(
+#'     title = "SARS-CoV-2",
+#'     subtitle = "Data Source: WashU Virus Genome Browser, NCBI, GISAID",
+#'     assembly = list(list("NC_045512.2", 29903)),
+#'     layout = "linear",
+#'     spacing = 50,
+#'     views = list(view2),
+#'     listify = FALSE
+#'   )
+#'
+#'   ui <- fluidPage(
+#'     use_gosling(),
+#'     fluidRow(
+#'       column(6, goslingOutput("gosling_plot"))
+#'     )
+#'   )
+#'
+#'
+#'   server <- function(input, output, session) {
+#'     output$gosling_plot <- renderGosling({
+#'       gosling(
+#'         component_id = "sars_cov2",
+#'         combined_view
+#'       )
+#'     })
+#'   }
+#'
+#'   shinyApp(ui, server)
+#'
+#' }
+#'
 #'
 #' @details For more info visit
 #' http://gosling-lang.org/docs/visual-channel#text
@@ -166,7 +587,6 @@ visual_channel_text <- function(
     field = field, type = type, range = range, domain = domain, ...
   )
 }
-
 
 #' color visual channel
 #'
@@ -184,6 +604,135 @@ visual_channel_text <- function(
 #' @param aggregate A character. One of "max", "min", "mean", "bin", "count".
 #' Specify how to aggregate data. Default: undefined.
 #' @param ... Any other parameters to pass to gosling.js.
+#' @examples
+#' if(interactive()) {
+#'   library(shiny)
+#'   library(shiny.gosling)
+#'
+#'   # View 2 Track 3----
+#'   view2_track3_data <- track_data(
+#'     url = "https://server.gosling-lang.org/api/v1/tileset_info/?d=NC_045512_2-multivec",
+#'     type = "multivec",
+#'     row = "base",
+#'     column = "position",
+#'     value = "count",
+#'     categories = c("A", "T", "G", "C"),
+#'     start = "start",
+#'     end = "end"
+#'   )
+#'
+#'   view2_track3a <- add_single_track(
+#'     mark = "bar",
+#'     y = visual_channel_y(
+#'       field = "count", type = "quantitative", axis = "none"
+#'     )
+#'   )
+#'
+#'   view2_track3b <- add_single_track(
+#'     dataTransform = track_data_transform(
+#'       type = "filter",
+#'       field = "count",
+#'       oneOf = list(0),
+#'       not = TRUE
+#'     ),
+#'     mark = "text",
+#'     x = visual_channel_x(
+#'       field = "start", type = "genomic"
+#'     ),
+#'     xe = visual_channel_x(
+#'       field = "end", type = "genomic"
+#'     ),
+#'     size = 24,
+#'     color = "white",
+#'     visibility = list(list(
+#'       operation = "less-than",
+#'       measure = "width",
+#'       threshold = "|xe-x|",
+#'       transitionPadding = 30,
+#'       target = "mark"
+#'     ),
+#'     list(
+#'       operation = "LT",
+#'       measure = "zoomLevel",
+#'       threshold = 40,
+#'       target = "track"
+#'     ))
+#'   )
+#'
+#'   view2_track3_x <- visual_channel_x(
+#'     field = "position", type = "genomic"
+#'   )
+#'
+#'   view2_track3_color <- visual_channel_color(
+#'     field = "base",
+#'     type = "nominal",
+#'     domain = c("A", "T", "G", "C"),
+#'     legend = TRUE
+#'   )
+#'
+#'   view2_track3_text <- visual_channel_text(
+#'     field = "base", type = "nominal"
+#'   )
+#'
+#'   view2_track3_style <- default_track_styles(
+#'     inlineLegend = TRUE
+#'   )
+#'
+#'   view2_track3 <- add_single_track(
+#'     title = "NC_045512.2 Sequence",
+#'     alignment = "overlay",
+#'     data = view2_track3_data,
+#'     tracks = add_multi_tracks(
+#'       view2_track3a, view2_track3b
+#'     ),
+#'     x = view2_track3_x,
+#'     color = view2_track3_color,
+#'     text = view2_track3_text,
+#'     style = view2_track3_style,
+#'     width = 800, height = 40
+#'   )
+#'
+#'   view2 <- compose_view(
+#'     multi = TRUE,
+#'     centerRadius = 0,
+#'     xDomain = list(interval = c(1, 29903)),
+#'     linkingId = "detail",
+#'     alignment = "stack",
+#'     tracks = add_multi_tracks(
+#'       view2_track3
+#'     )
+#'   )
+#'
+#'   combined_view <- arrange_views(
+#'     title = "SARS-CoV-2",
+#'     subtitle = "Data Source: WashU Virus Genome Browser, NCBI, GISAID",
+#'     assembly = list(list("NC_045512.2", 29903)),
+#'     layout = "linear",
+#'     spacing = 50,
+#'     views = list(view2),
+#'     listify = FALSE
+#'   )
+#'
+#'   ui <- fluidPage(
+#'     use_gosling(),
+#'     fluidRow(
+#'       column(6, goslingOutput("gosling_plot"))
+#'     )
+#'   )
+#'
+#'
+#'   server <- function(input, output, session) {
+#'     output$gosling_plot <- renderGosling({
+#'       gosling(
+#'         component_id = "sars_cov2",
+#'         combined_view
+#'       )
+#'     })
+#'   }
+#'
+#'   shinyApp(ui, server)
+#'
+#' }
 #'
 #' @details For more info visit
 #' http://gosling-lang.org/docs/visual-channel#color
