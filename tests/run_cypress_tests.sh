6fe70f668cba308f9679f636bc9b75e9d447be70
federiva <federivadeneira@gmail.com>
2023-07-24 20:50:15 -0300

    feat: adding script to run all of the tests

---
+++ tests/run_cypress_tests.sh
@@ -0,0 +1,14 @@
+yarn e2e-circularLinearWithBrush
+yarn e2e-barChart
+yarn e2e-areaChart
+yarn e2e-ideogram
+yarn e2e-circularVisualEncoding
+yarn e2e-multiTrackApp
+yarn e2e-pointPlot
+yarn e2e-ruleMarks
+yarn e2e-sarsCov2
+yarn e2e-lineChart
+yarn e2e-staticCircularBar
+yarn e2e-structuralVariant
+yarn e2e-textAnnotation
+yarn e2e-textMarks
