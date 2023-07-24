01e9192baaf638b8f90295936b5c71571ec4d911
federiva <federivadeneira@gmail.com>
2023-07-24 08:02:00 -0300

    feat: Adding e2e test for circularLinearWithBrush example

---
+++ tests/cypress/e2e/circularLinearWithBrushSpec.cy.js
@@ -0,0 +1,34 @@
+describe('circularLinearWithBrush Snapshots', () => {
+  it('Screenshots are matching up', () => {
+    cy.visit('http://localhost:8888')
+      .then(() => {
+        // cy.get('#gosling_plot_circular .gosling-component', { timeout: 10000 })
+          // .should('be.visible')
+          cy.wait(10000);
+
+          cy.get(".center-track")
+            .should('have.length', 3)
+            .each( ($element, index, $list) => {
+              cy.wrap($element).should('be.visible').toMatchImageSnapshot();
+            })
+          
+          // Interact with selectizeInput
+          cy.get('.selectize-control').click();
+          cy.get('div[data-value="chrX"]').click();
+          cy.get('#go_to_chr').click();
+          
+          cy.wait(5000); // TODO (Fede Jul 24 2023) Remove explicit waits when we found an onRender/onAnimateTransform, etc.
+          // event to listen to
+          cy.get(".center-track")
+            .should('have.length', 3)
+            .each( ($element, index, $list) => {
+              const snapshotName = `circular-linear-with-brush-snapshot-track-${index}`
+              cy.wrap($element).should('be.visible').toMatchImageSnapshot(name = snapshotName);
+            })
+      });
+  })
+
+  // it('Chromosome selector works', () => {
+  //   cy.visit("")
+  // })
+});
