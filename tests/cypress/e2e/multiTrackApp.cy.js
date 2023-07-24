9320bf0275acba837ef9026998963acc0ac4b24d
federiva <federivadeneira@gmail.com>
2023-07-24 20:25:22 -0300

    feat: adding multitrack

---
+++ tests/cypress/e2e/multiTrackApp.cy.js
@@ -0,0 +1,15 @@
+describe('multiTrackApp Snapshots', () => {
+    it('Screenshots are matching up', () => {
+      cy.visit('http://localhost:8888')
+        .then(() => {
+            cy.wait(10000); // TODO (Fede Jul 24 2023) Remove explicit waits when we found an onRender/onAnimateTransform, etc.
+  
+            cy.get(".center-track")
+              .should('have.length', 1)
+              .each( ($element, index, $list) => {
+                cy.wrap($element).should('be.visible').toMatchImageSnapshot();
+              })
+        });
+    })
+  });
+  
---
+++ tests/cypress/r_scripts/run_multiTrackApp.R
@@ -0,0 +1,2 @@
+devtools::load_all()
+shiny::shinyAppDir("../inst/examples/multiTrackApp", options = list(port = 8888))
