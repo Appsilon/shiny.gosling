8b84fc353cde120bcd74105fe382a990bc742fcc
federiva <federivadeneira@gmail.com>
2023-07-24 20:35:12 -0300

    tests: Adding sarsCov

---
+++ tests/cypress/e2e/sarsCov2.cy.js
@@ -0,0 +1,15 @@
+describe('sarsCov2 Snapshots', () => {
+    it('Screenshots are matching up', () => {
+      cy.visit('http://localhost:8888')
+        .then(() => {
+            cy.wait(20000); // TODO (Fede Jul 24 2023) Remove explicit waits when we found an onRender/onAnimateTransform, etc.
+  
+            cy.get(".center-track")
+              .should('have.length', 5)
+              .each( ($element, index, $list) => {
+                cy.wrap($element).should('be.visible').toMatchImageSnapshot();
+              })
+        });
+    })
+  });
+  
---
+++ tests/cypress/r_scripts/run_sarsCov2.R
@@ -0,0 +1,2 @@
+devtools::load_all()
+shiny::shinyAppDir("../inst/examples/sarsCov2/", options = list(port = 8888))
