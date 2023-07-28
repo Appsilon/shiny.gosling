307ae8b6543fca9e5c3efa69d0dbdaaf283e9268
federiva <federivadeneira@gmail.com>
2023-07-28 15:46:49 -0300

    fix: Avoid appending

--- tests/cypress/update_run_all_tests.sh
+++ tests/cypress/update_run_all_tests.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep -o 'e2e-[a-zA-Z0-9]*' ../package.json | sed 's/^/yarn /' >> ../run_cypress_tests.sh
+grep -o 'e2e-[a-zA-Z0-9]*' ../package.json | sed 's/^/yarn /' > ../run_cypress_tests.sh
