555c99177e6e2268ae2b12251e183b8a21039b2d
federiva <federivadeneira@gmail.com>
2023-07-30 23:33:17 -0300

    feat: Adding port

--- tests/cypress/create_tests_templates.sh
+++ tests/cypress/create_tests_templates.sh
@@ -9,6 +9,7 @@ custom_text="$1"
 cy_spec_out="$1.cy.js"
 r_script_out="run_$1.R"
 package_json=../package.json
+default_port=8888
 
 # Escape special characters in the custom_text to use in sed
 escaped_custom_text=$(printf '%s\n' "$custom_text" | sed -e 's/[\/&]/\\&/g')
@@ -17,8 +18,8 @@ escaped_custom_text=$(printf '%s\n' "$custom_text" | sed -e 's/[\/&]/\\&/g')
 cat "r_scripts/template.R" | sed "s/<<<var_1>>>/$escaped_custom_text/g"  >> r_scripts/$r_script_out
 cat "e2e/template.template" | sed "s/<<<var_1>>>/$escaped_custom_text/g"  >> e2e/$cy_spec_out
 
-rscript_command="Rscript cypress/r_scripts/$r_script_out"
-e2e_command="start-server-and-test $escaped_custom_text http://localhost:8888 'cypress run --spec \"cypress/e2e/$cy_spec_out\" --record false'"
+rscript_command="Rscript cypress/r_scripts/$r_script_out $default_port"
+e2e_command="start-server-and-test $escaped_custom_text http://localhost:$default_port 'cypress run --spec \"cypress/e2e/$cy_spec_out\" --record false'"
 
 echo $rscript_command
 # Add the entry to scripts section in package.json
